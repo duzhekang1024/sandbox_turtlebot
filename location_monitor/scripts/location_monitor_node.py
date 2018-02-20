007e96092c7a4ae3d17dd57baee466f0d6c3814e
Zhekang Du <duzhekang1024@gmail.com>
2018-02-20 10:34:52 -0800

    update

--- location_monitor/scripts/location_monitor_node.py
+++ location_monitor/scripts/location_monitor_node.py
@@ -6,42 +6,47 @@ from nav_msgs.msg import Odometry
 from location_monitor.msg import LandmarkDistance
 
 def distance(x1, y1, x2, y2):
-	xd = x1 - x2
-	yd = y1 - y2
-	return math.sqrt(xd*xd + yd*yd)
+    xd = x1 - x2
+    yd = y1 - y2
+    return math.sqrt(xd*xd + yd*yd)
 
 class LandmarkMonitor(object):
-        def __init__(self, pub, landmarks):
-                self._pub = pub
-                self._landmarks = landmarks
+    def __init__(self, pub, landmarks):
+        self._pub = pub
+        self._landmarks = landmarks
 
-	def callback(self, msg):
-		x = msg.pose.pose.position.x
-		y = msg.pose.pose.position.y
-		closest_name = None
-		closest_distance = None
-		for l_name, l_x, l_y in self._landmarks:	
-			dist = distance(x, y, l_x, l_y)
-			if closest_distance is None or dist < closest_distance:
-				closest_name = l_name
-				closest_distance = dist
-		rospy.loginfo('closest: {}'. format(closest_name))
+    def callback(self, msg):
+	x = msg.pose.pose.position.x
+	y = msg.pose.pose.position.y
+	closest_name = None
+	closest_distance = None
+	for l_name, l_x, l_y in self._landmarks:
+	    dist = distance(x, y, l_x, l_y)
+	    if closest_distance is None or dist < closest_distance:
+		closest_name = l_name
+		closest_distance = dist
+        ld = LandmarkDistance()
+        ld.name = closest_name
+        ld.distance = closest_distance
+        self._pub.publish(ld)
+        if closest_distance < 0.5:
+            rospy.loginfo('I\'m near the {}'. format(closest_name))
 
 def main():
-	rospy.init_node('location_monitor')
-	landmarks = []
-	landmarks.append(("Cube", 0.31, -0.99));
-	landmarks.append(("DumpsterCube", 0.11, -2.42));
-	landmarks.append(("Cylinder", -1.14, -2.88));
-	landmarks.append(("Barrier", -2.59, -0.83));
-	landmarks.append(("Bookshelf", -0.09, 0.53));
-	
-	pub = rospy.Publisher('closet_lanmark',LandmarkDistance)
-	monitor = LandmarkMonitor(pub, landmarks)
+    rospy.init_node('location_monitor')
+    landmarks = []
+    landmarks.append(("Cube", 0.31, -0.99));
+    landmarks.append(("DumpsterCube", 0.11, -2.42));
+    landmarks.append(("Cylinder", -1.14, -2.88));
+    landmarks.append(("Barrier", -2.59, -0.83));
+    landmarks.append(("Bookshelf", -0.09, 0.53));
 
-	rospy.Subscriber("/odom", Odometry, monitor.callback)
-	rospy.spin()
-	
+    pub = rospy.Publisher('closest_landmark',LandmarkDistance, queue_size=10)
+    monitor = LandmarkMonitor(pub, landmarks)
+
+    rospy.Subscriber("/odom", Odometry, monitor.callback)
+    rospy.spin()
+
 
 if __name__ == '__main__':
-	main()
+    main()
